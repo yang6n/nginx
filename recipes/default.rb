0fb94fb6521a82c3f14f851fd2608990848a2705
Yang Jian <yang6n@users.noreply.github.com>
2017-01-06 17:02:02 -0600

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,41 @@
-#
-# Cookbook Name:: nginx
-# Recipe:: default
-#
+# This is an example recipe only, not meant to be used for production deployment.
+# This should serve as a simple guide of how to use the resource to set up nginx.
+# See more in the README.md and in test/fixtures/cookbooks for more examples.
 
-# default recipe is intentionally left blank
+nginx_service 'example'
+
+root_dir = '/var/www/example'
+
+directory root_dir do
+  recursive true
+end
+
+file File.join(root_dir, 'index.htm') do
+  content <<-EOF.gsub(/^ {4}/, '')
+    <html>
+    <body>
+    Hello, world!
+    </body>
+    </html
+  EOF
+end
+
+# @todo convert this to a proper nginx_config resource
+file '/etc/nginx-example/sites-enabled/default' do
+  content <<-EOF.gsub(/^ {4}/, '')
+    server {
+      listen 80 default_server;
+      listen [::]:80 default_server ipv6only=on;
+      root #{root_dir};
+      index index.html index.htm;
+      # Make site accessible from http://localhost/
+      server_name localhost;
+      location / {
+        # First attempt to serve request as file, then
+        # as directory, then fall back to displaying a 404.
+        try_files $uri $uri/ =404;
+      }
+    }
+  EOF
+  notifies :reload, 'nginx_service[example]'
+end
